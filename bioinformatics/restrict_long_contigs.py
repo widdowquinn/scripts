a73e797c00c91d7f184a955c8845949b6512e95d
widdowquinn <leightonpritchard@mac.com>
2014-02-01 13:01:38 +0000

    PEP8 corrections and usage information to restrict_long_contigs.py

--- bioinformatics/restrict_long_contigs.py
+++ bioinformatics/restrict_long_contigs.py
@@ -2,11 +2,20 @@
 #
 # restrict_long_contigs.py
 #
-# USAGE: 
+# USAGE: restrict_long_contigs.py [options] <input_directory> \
+#                                       <output_directory>
+#
+# Options:
+#   -h, --help            show this help message and exit
+#   -l MINLEN, --minlen=MINLEN
+#                         Minimum length of sequence
+#   -s SUFFIX, --filesuffix=SUFFIX
+#                         Suffix to indicate the file was processed
+#   -v, --verbose         Give verbose output
 #
 # Non-PSL dependencies: Biopython (www.biopython.org)
 #
-# A short script that takes as input a directory containing (many) FASTA files 
+# A short script that takes as input a directory containing (many) FASTA files
 # describing biological sequences, and writes to a new, named directory
 # multiple FASTA files containing the same sequences, but restricted only to
 # those sequences whose length is greater than a passed value.
@@ -62,12 +71,13 @@ import sys
 # File extensions that indicate FASTA content
 fasta_ext = ['.fa', '.fas', '.fasta']
 
+
 ###
 # FUNCTIONS
 
 # Parse cmd-line
 def parse_cmdline(args):
-    """ Parse command-line arguments. Note that the input and output 
+    """ Parse command-line arguments. Note that the input and output
         directories are positional arguments
     """
     usage = "usage: %prog [options] <input_directory> <output_directory>"
@@ -83,14 +93,15 @@ def parse_cmdline(args):
                       help="Give verbose output")
     return parser.parse_args()
 
+
 # Get list of FASTA files from a directory
 def get_fasta_filenames(indir, extensions=fasta_ext):
     """ Identifies files in the passed directory whose extensions indicate
         that they may be FASTA files. Returns the path to the file,
         including the parent directory.
     """
-    filelist = [f for f in os.listdir(indir) if \
-                    os.path.splitext(f)[-1].lower() in extensions]
+    filelist = [f for f in os.listdir(indir) if
+                os.path.splitext(f)[-1].lower() in extensions]
     logger.info("Identified %d FASTA files in %s:" % (len(filelist),
                                                       indir))
     if not len(filelist):       # We want there to be at least one file
@@ -98,21 +109,23 @@ def get_fasta_filenames(indir, extensions=fasta_ext):
         sys.exit(1)
     return filelist
 
-# Restrict sequence length in a named FASTA file, writing it to 
+
+# Restrict sequence length in a named FASTA file, writing it to
 # the named location
 def restrict_seq_length(infile, outfile, minlen):
     """ Takes an input FASTA file as infile, and writes out a corresponding
         file to outfile, where sequences shorter than minlen are not included
     """
-    logger.info("Restricting lengths of %s to >=%d;" % (infile,minlen)+\
-                    " writing to %s" % outfile)
-    SeqIO.write([s for s in SeqIO.parse(infile, 'fasta') \
-                     if not len(s) < minlen], 
+    logger.info("Restricting lengths of %s to >=%d;" % (infile, minlen) +
+                " writing to %s" % outfile)
+    SeqIO.write([s for s in SeqIO.parse(infile, 'fasta')
+                 if not len(s) < minlen],
                 outfile, 'fasta')
+
 
 # Process FASTA files in the directory
 def process_files(indir, outdir, minlen, suffix):
-    """ Takes an input directory that contains FASTA files, and writes 
+    """ Takes an input directory that contains FASTA files, and writes
         to the output directory corresponding files (with the suffix appended)
         that contain only sequences of length greater than minlen.
     """
@@ -121,24 +134,23 @@ def process_files(indir, outdir, minlen, suffix):
         infilename = os.path.join(indir, filename)
         outfilename = os.path.join(outdir, ''.join([filestem, suffix, ext]))
         restrict_seq_length(infilename, outfilename, minlen)
-    
-                              
+
+
 ###
 # SCRIPT
 
 if __name__ == '__main__':
-                                  
+
     # Parse command-line
     # options are options, arguments are the .sff files
     options, args = parse_cmdline(sys.argv)
-    
+
     # We set up logging, and modify loglevel according to whether we need
     # verbosity or not
     logger = logging.getLogger('restrict_long_contigs.py')
     logger.setLevel(logging.DEBUG)
     err_handler = logging.StreamHandler(sys.stderr)
-    err_formatter = \
-                  logging.Formatter('%(levelname)s: %(message)s')
+    err_formatter = logging.Formatter('%(levelname)s: %(message)s')
     err_handler.setFormatter(err_formatter)
     if options.verbose:
         err_handler.setLevel(logging.INFO)
@@ -152,8 +164,8 @@ if __name__ == '__main__':
 
     # If there are not two positional arguments, throw an error
     if len(args) != 2:
-        logger.error("Not enough arguments: script requires input and " +\
-                         "output directory")
+        logger.error("Not enough arguments: script requires input and " +
+                     "output directory")
         sys.exit(1)
     indir, outdir = tuple(args)
 
@@ -161,16 +173,16 @@ if __name__ == '__main__':
     if not os.path.isdir(indir):
         logger.error("Input directory %s does not exist" % indir)
         sys.exit(1)
-    
-    # If output directory does not exist, create it. If it does exist, 
+
+    # If output directory does not exist, create it. If it does exist,
     # issue a warning that contents may be overwritten
     if os.path.isdir(outdir):
         logger.warning("Contents of %s may be overwritten" % outdir)
     else:
-        logger.warning("Output directory %s does not exist: creating it" % \
-                           outdir)
+        logger.warning("Output directory %s does not exist: creating it" %
+                       outdir)
         os.mkdir(outdir)
-        
+
     # Check that the passed suffix is a valid string: escape dodgy characters
     #try:
     #    suffix = re.escape(options.suffix)
@@ -180,9 +192,9 @@ if __name__ == '__main__':
 
     # Make sure that the minimum length is an integer, and positive
     if not int(options.minlen) > 0:
-        logger.error("Minimum length must be a positive integer, got %s" % \
-                         options.minlen)
+        logger.error("Minimum length must be a positive integer, got %s" %
+                     options.minlen)
         sys.exit(1)
-        
+
     # Restrict sequence lengths
     process_files(indir, outdir, int(options.minlen), options.suffix)
