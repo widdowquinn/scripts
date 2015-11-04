c9a4a149e4766e57a9b456c604e7d0eff9fd6a00
widdowquinn <leighton.pritchard@hutton.ac.uk>
2015-11-04 11:10:14 +0000

    genbank_get_genomes_by_taxon.py: Added script framework

---
+++ bioinformatics/genbank_get_genomes_by_taxon.py
@@ -0,0 +1,132 @@
+#/usr/bin/env python
+#
+# genbank_get_genomes_by_taxon.py
+#
+# A script that takes an NCBI taxonomy identifier (or string, though this is
+# not reliable for taxonomy tree subgraphs...) and downloads all genomes it 
+# can find from NCBI in the corresponding taxon subgraph with the passed
+# argument as root.
+#
+# (c) TheJames Hutton Institute 2015
+# Author: Leighton Pritchard
+
+import logging
+import logging.handlers
+import os
+import shutil
+import sys
+import time
+import traceback
+
+from argparse import ArgumentParser
+
+from Bio import Entrez, SeqIO
+
+# Parse command-line
+def parse_cmdline(args):
+    """Parse command-line arguments"""
+    parser = ArgumentParser(usage)
+    parser.add_option("-o", "--outdir", dest="outdirname",
+                      action="store", default=None,
+                      help="Output directory")
+    parser.add_option("-t", "--taxon", dest="taxon",
+                      action="store", default=None,
+                      help="NCBI taxonomy ID")
+    parser.add_option("-v", "--verbose", dest="verbose",
+                      action="store_true", default=False,
+                      help="Give verbose output")
+    parser.add_option("-f", "--force", dest="force",
+                      action="store_true", default=False,
+                      help="Force file overwriting")
+    parser.add_argument("--noclobber", dest="noclobber",
+                        action="store_true", default=False,
+                        help="Don't nuke existing files")
+    parser.add_option("-l", "--logfile", dest="logfile",
+                      action="store", default=None,
+                      help="Logfile location")
+    parser.add_option("--format", dest="format",
+                      action="store", default="gbk,fasta",
+                      help="Output file format [gbk|fasta]")
+    return parser.parse_args()
+
+
+# Create output directory if it doesn't exist
+def make_outdir():
+    """Make the output directory, if required.
+    This is a little involved.  If the output directory already exists,
+    we take the safe option by default, and stop with an error.  We can,
+    however, choose to force the program to go on, in which case we can
+    either clobber the existing directory, or not.  The options turn out
+    as the following, if the directory exists:
+    DEFAULT: stop and report the collision
+    FORCE: continue, and remove the existing output directory
+    NOCLOBBER+FORCE: continue, but do not remove the existing output
+    """
+    if os.path.exists(args.outdirname):
+        if not args.force:
+            logger.error("Output directory %s would " % args.outdirname +
+                         "overwrite existing files (exiting)")
+            sys.exit(1)
+        else:
+            logger.info("Removing directory %s and everything below it" %
+                        args.outdirname)
+            if args.noclobber:
+                logger.warning("NOCLOBBER: not actually deleting directory")
+            else:
+                shutil.rmtree(args.outdirname)
+    logger.info("Creating directory %s" % args.outdirname)
+    try:
+        os.makedirs(args.outdirname)   # We make the directory recursively
+    except OSError:
+        # This gets thrown if the directory exists. If we've forced overwrite/
+        # delete and we're not clobbering, we let things slide
+        if args.noclobber and args.force:
+            logger.info("NOCLOBBER+FORCE: not creating directory")
+        else:
+            logger.error(last_exception)
+            sys.exit(1)
+
+
+# Run as script
+if __name__ == '__main__':
+
+    # Parse command-line
+    args = parse_cmdline(sys.argv)
+
+    # Set up logging
+    logger = logging.getLogger('genbank_get_genomes_by_taxon.py')
+    logger.setLevel(logging.DEBUG)
+    err_handler = logging.StreamHandler(sys.stderr)
+    err_formatter = logging.Formatter('%(levelname)s: %(message)s')
+    err_handler.setFormatter(err_formatter)
+
+    # Was a logfile specified? If so, use it
+    if args.logfile is not None:
+        try:
+            logstream = open(args.logfile, 'w')
+            err_handler_file = logging.StreamHandler(logstream)
+            err_handler_file.setFormatter(err_formatter)
+            err_handler_file.setLevel(logging.INFO)
+            logger.addHandler(err_handler_file)
+        except:
+            logger.error("Could not open %s for logging" %
+                         args.logfile)
+            sys.exit(1)
+
+    # Do we need verbosity?
+    if args.verbose:
+        err_handler.setLevel(logging.INFO)
+    else:
+        err_handler.setLevel(logging.WARNING)
+    logger.addHandler(err_handler)
+
+    # Report arguments, if verbose
+    logger.info(args)
+
+    # Have we got an output directory? If not, exit.
+    if args.outdirname is None:
+        logger.error("No output directory name (exiting)")
+        sys.exit(1)
+    make_outdir()
+    logger.info("Output directory: %s" % args.outdirname)
+
